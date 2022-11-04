ccb69d869aecacb7af468c1843f4fe7157147180
unknown <ivnymetzker@gmail.com>
2022-11-04 19:23:10 -0300

    Aula 02 - Desafio de código

---
+++ JavaScript/aula02Desafio.js
@@ -0,0 +1,16 @@
+
+// Declaração de variáveis
+
+const etanol = 3.80;
+const gasolina = 5.79;
+const combustivel = kmgasolina;
+const kmlt = 17;
+const km = 400;
+const valorviagem = 0;
+
+if (combustivel === kmgasolina){
+    valorviagem = (km / kmlt) * gasolina;
+    console.log ('O valor da gasolina gasta na viagem será de: ', valorviagem)
+}
+
+
