5a0fea69b8399d61dc105121f9aa5c05e6d62376
unknown <ivnymetzker@gmail.com>
2022-11-04 23:00:09 -0300

    Aula 02 - Desafio de´código, aprimorando com uma string

--- JavaScript/aula02Desafio.js
+++ JavaScript/aula02Desafio.js
@@ -3,15 +3,18 @@
 
 const etanol = 3.80;
 const gasolina = 5.79;
-const combustivel = 0;
+const combustivel = 'etanol';
 const kmlt = 17;
 const km = 400;
 
- if (combustivel === 1){
+
+// Realizando uma condicional para cálculo do valor gasto de combustível com etanol ou gasolina
+
+ if (combustivel === 'etanol'){
     const valorviagem = (km / kmlt) * etanol;
     console.log ('O valor do etanol gasto na viagem será de: R$ ', valorviagem.toFixed(2));
 }
-else if (combustivel === 2){
+else if (combustivel === 'gasolina'){
     const valorviagem = (km / kmlt) * gasolina;
     console.log ('O valor da gasolina gasta na viagem será de: R$ ', valorviagem.toFixed(2));
 }
