07b5e96f16481d782596f8dd37cd0761ab7a6915
unknown <ivnymetzker@gmail.com>
2022-11-04 23:47:42 -0300

    Desafio 2 - Aprimoramento do código

--- JavaScript/aula03Desafio.js
+++ JavaScript/aula03Desafio.js
@@ -1,18 +1,15 @@
-const nota01 = 9;
+const nota01 = 8;
 const nota02 = 9;
-const nota03 = 9;
+const nota03 = 7;
 const media = (nota01 + nota02 + nota03) / 3;
 
 
-if (media > 7){
+if (media > 7) {
     console.log('Parabéns! Você está APROVADO! Sua média foi: ', media.toFixed(1));
 }
-else if (media === 5 | media > 5){
+else if (media >= 5 && media <= 7) {
     console.log('Por pouco! Você está de RECUPERAÇÃO. Sua média foi: ', media.toFixed(1));
 }
-else if (media === 7 | media > 7){
-    console.log('Por pouco! Você está de RECUPERAÇÃO. Sua média foi: ', media.toFixed(1));
-}
-else{
+else {
     console.log('Precisa se esforçar mais! Você foi REPROVADO. Sua média foi: ', media.toFixed(1));
 }
