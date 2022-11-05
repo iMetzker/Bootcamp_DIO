fa34e62fedbfa85ac8c13a5c9aa92dbce3a71b3e
unknown <ivnymetzker@gmail.com>
2022-11-05 18:24:59 -0300

    Desafio 3 - Aprimoramento do código

--- JavaScript/aula04Desafio.js
+++ JavaScript/aula04Desafio.js
@@ -3,20 +3,20 @@
 const peso = 71.3;
 const altura = 1.74;
 
-const imc = peso / (altura * altura);
+const imc = peso / Math.pow(altura, 2);
 
-if(imc < 18.5){
+if (imc < 18.5) {
     console.log('Você está abaixo do peso, procure um profissional. Seu IMC é: ', imc.toFixed(1));
 }
-else if(imc >= 18.5 && imc < 25){
+else if (imc >= 18.5 && imc < 25) {
     console.log('Parabéns! Você está no seu peso ideal. Seu IMC é: ', imc.toFixed(1));
 }
-else if( imc >= 25 && imc < 30){
+else if (imc >= 25 && imc < 30) {
     console.log('Você está acima do peso, procuse um profissional. Seu IMC é: ', imc.toFixed(1));
 }
-else if(imc >= 30 && imc < 40){
+else if (imc >= 30 && imc < 40) {
     console.log('Você está obeso, procuse um profissional. Seu IMC é: ', imc.toFixed(1));
 }
-else{
+else {
     console.log('Você está com Obesidade Grave, procuse um profissional. Seu IMC é: ', imc.toFixed(1));
 }
