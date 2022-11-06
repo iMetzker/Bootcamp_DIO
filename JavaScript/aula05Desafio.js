37dd560c0f6e6bbe8904328eb300b1d9038324a2
unknown <ivnymetzker@gmail.com>
2022-11-06 16:16:13 -0300

    Desafio 4 - Aprimoramento de código

--- JavaScript/aula05Desafio.js
+++ JavaScript/aula05Desafio.js
@@ -3,18 +3,18 @@ const pagamento = 'debito';
 let receber = 0;
 
 if (pagamento === 'debito') {
-    receber = produto - ((produto * 10) / 100);
+    receber = produto - (produto * 0.1);
     console.log('O valor a ser pago no débito é de R$ ', receber);
 }
 else if (pagamento === 'dinheiro' | 'pix') {
-    receber = produto - ((produto * 15) / 100);
+    receber = produto - (produto * 0.15);
     console.log('O valor a ser pago no dinheiro ou por PIX é de R$ ', receber);
 }
 else if (pagamento === 'dividir2x') {
     console.log('O valor total a ser pago dividindo em 2x é de R$ ', produto);
 }
 else if (pagamento === 'dividir2+') {
-    receber = produto + ((produto * 10) / 100);
+    receber = produto + (produto * 0.1);
     console.log('O valor total a ser pago dividindo em mais de 2x é de R$ ', receber);
 }
 else {
